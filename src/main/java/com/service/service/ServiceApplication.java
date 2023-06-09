f88fedfb1cac0af1447f3d3a0b00235dd498a349
sya17 <sariphidayatullah170701@gmail.com>
2023-06-09 23:23:02 +0700

    Added Swagger Documentation

--- src/main/java/com/service/service/ServiceApplication.java
+++ src/main/java/com/service/service/ServiceApplication.java
@@ -1,10 +1,7 @@
 package com.service.service;
 
-import org.modelmapper.Conditions;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -12,13 +9,4 @@ public class ServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
-
-	// @Bean
-	// public ModelMapper modelMapper() {
-    //     ModelMapper mp = new ModelMapper();
-    //     mp.getConfiguration()
-	// 	.setSkipNullEnabled(true)
-	// 	.setPropertyCondition(Conditions.isNotNull());
-	// 	return mp;
-	// }
 }
